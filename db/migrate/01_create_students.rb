1f0328b44a65fd9c1a09ac5408b2e91bee61d2b2
Joe Ganobsik <jganobsik@gmail.com>
2019-01-09 19:51:03 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,9 +1,5 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
-  
-  ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/students.sqlite"
-)
+
 
  create_table :students do |t|
       t.string :name
